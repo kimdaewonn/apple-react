aa3fbe6acbed5cbbb32c274c94a0e9292c59e69c
kimdaewonn <eodnjs9605@gmail.com>
2023-01-19 23:15:52 +0900

    state toggle

--- src/App.js
+++ src/App.js
@@ -8,27 +8,10 @@ function App() {
         "파이썬독학",
     ]);
     let [따봉, 따봉변경] = useState(0);
+    let [modal, setModal] = useState(false);
 
     return (
         <div className="App">
-            <button
-                onClick={() => {
-                    let copy = [...글제목];
-                    copy[0] = "여자 코트 추천";
-                    글제목변경(copy);
-                }}
-            >
-                글수정
-            </button>
-            <button
-                onClick={() => {
-                    let copy = [...글제목];
-                    copy.sort();
-                    글제목변경(copy);
-                }}
-            >
-                가나다순
-            </button>
             <div className="list">
                 <h4>
                     {글제목[0]}
@@ -48,11 +31,27 @@ function App() {
                 <p>2월 17일 발행</p>
             </div>
             <div className="list">
-                <h4>{글제목[2]}</h4>
+                <h4
+                    onClick={() => {
+                        setModal(!modal);
+                    }}
+                >
+                    {글제목[2]}
+                </h4>
                 <p>2월 17일 발행</p>
             </div>
+            {modal === true ? <Modal /> : null}
+        </div>
+    );
+}
+
+function Modal() {
+    return (
+        <div className="modal">
+            <h4>제목</h4>
+            <p>날짜</p>
+            <p>상세내용</p>
         </div>
     );
 }
 export default App;
-main main
