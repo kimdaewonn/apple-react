e7b022493b2973df9db2ef41aa0fce625e86d3d3
kimdaewonn <eodnjs9605@gmail.com>
2023-01-23 00:09:01 +0900

    props 숙제완료

--- src/App.js
+++ src/App.js
@@ -18,36 +18,53 @@ function App() {
                         <div className="list" key={i}>
                             <h4
                                 onClick={() => {
-                                    let copy = [...따봉];
-                                    copy[i] = copy[i] + 1;
-                                    따봉변경(copy);
+                                    setModal(!modal);
                                 }}
                             >
-                                {글제목[i]}★{따봉[i]}
+                                <span
+                                    onClick={() => {
+                                        let copy = [...따봉];
+                                        copy[i] = copy[i] + 1;
+                                        따봉변경(copy);
+                                    }}
+                                >
+                                    {글제목[i]}★{따봉[i]}
+                                </span>
                             </h4>
+
                             <p>2월 18일 발행</p>
                         </div>
                     );
                 })}
             </div>
-            <button
-                onClick={() => {
-                    setModal(!modal);
-                }}
-            >
-                {글제목[0]}
-            </button>
-            <div>{modal === true ? <Modal /> : null}</div>
+            <div>
+                {modal === true ? (
+                    <Modal
+                        color={"yellow"}
+                        글제목변경={글제목변경}
+                        글제목={글제목}
+                    />
+                ) : null}
+            </div>
         </>
     );
 }
 
-function Modal() {
+function Modal(props) {
     return (
-        <div className="modal">
-            <h4>제목</h4>
+        <div className="modal" style={{ background: props.color }}>
+            <h4>{props.글제목[0]}</h4>
             <p>날짜</p>
             <p>상세내용</p>
+            <button
+                onClick={() => {
+                    let copy = [...props.글제목];
+                    copy[0] = "여자코트추천";
+                    props.글제목변경(copy);
+                }}
+            >
+                글수정
+            </button>
         </div>
     );
 }
