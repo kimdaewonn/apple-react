9fef050874dd8583b80d5c17e2a0a083ff20dfe2
kimdaewonn <eodnjs9605@gmail.com>
2023-01-22 22:27:47 +0900

    배열복사 map

--- src/App.js
+++ src/App.js
@@ -7,41 +7,38 @@ function App() {
         "강남 우동맛집",
         "파이썬독학",
     ]);
-    let [따봉, 따봉변경] = useState(0);
+    let [따봉, 따봉변경] = useState([0, 0, 0]);
     let [modal, setModal] = useState(false);
 
     return (
-        <div className="App">
-            <div className="list">
-                <h4>
-                    {글제목[0]}
-                    <span
-                        onClick={() => {
-                            따봉변경(따봉 + 1);
-                        }}
-                    >
-                        👍
-                    </span>
-                    {따봉}
-                </h4>
-                <p>2월 17일 발행</p>
+        <>
+            <div>
+                {글제목.map(function (a, i) {
+                    return (
+                        <div className="list" key={i}>
+                            <h4
+                                onClick={() => {
+                                    let copy = [...따봉];
+                                    copy[i] = copy[i] + 1;
+                                    따봉변경(copy);
+                                }}
+                            >
+                                {글제목[i]}★{따봉[i]}
+                            </h4>
+                            <p>2월 18일 발행</p>
+                        </div>
+                    );
+                })}
             </div>
-            <div className="list">
-                <h4>{글제목[1]}</h4>
-                <p>2월 17일 발행</p>
-            </div>
-            <div className="list">
-                <h4
-                    onClick={() => {
-                        setModal(!modal);
-                    }}
-                >
-                    {글제목[2]}
-                </h4>
-                <p>2월 17일 발행</p>
-            </div>
-            {modal === true ? <Modal /> : null}
-        </div>
+            <button
+                onClick={() => {
+                    setModal(!modal);
+                }}
+            >
+                {글제목[0]}
+            </button>
+            <div>{modal === true ? <Modal /> : null}</div>
+        </>
     );
 }
 
